0567fd0bb5faee84ed33d3e4eac8049013b0e8b7
PgCola <lakai243@gmail.com>
2022-04-19 21:45:25 +0200

    Added handling of ctrl+d to close toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -26,6 +26,13 @@ toggleterm.setup({
 	},
 })
 
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-d>', [[<cmd>:q<cr>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 local Terminal = require("toggleterm.terminal").Terminal
 
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
